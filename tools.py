86332d652efc68837d63e801a9e69fea4b1209de
chabawiki <chabawiki@c4af189a-ae91-11dd-9763-1166fc48c086>
2008-11-10 17:05:19 +0000

    cleanup

---
+++ tools.py
@@ -0,0 +1,10 @@
+def urlencode(query):
+    """This can encode a query so that it can be sent as a query using
+       a http POST request"""
+    l=[]
+    for k, v in query:
+        k = urllib.quote(k)
+        v = urllib.quote(v)
+        l.append(k + '=' + v)
+    return '&'.join(l)
+
